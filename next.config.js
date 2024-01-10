1dc8dcb3fd8174690888cd78f23eab37697b75f1
rubyscore_tech@lansoft.dev <rubyscore_tech@lansoft.dev>
2024-01-10 22:47:31 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,35 +1,39 @@
-const API_URL = process.env.NEXT_PUBLIC_DEVELOP_PROXY_BACK_END_API
+const API_URL = process.env.NEXT_PUBLIC_DEVELOP_PROXY_BACK_END_API;
 
 const nextConfig = {
-    swcMinify: true,
-    async headers() {
-        return [
-            {
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Request-Headers", value: "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept" },
-                    { key: "Access-Control-Allow-Origin", value: "https://galxe.com" },
-                    { key: "Access-Control-Allow-Credentials", value: "false" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS,DELETE,PUT" },
-                ]
-            }
-        ]
-    },
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path(^(?!.*\\/static\\/).*$)',
-                destination: `/api/:path*`,
-            },
-            {
-                source: '/api/:path*',
-                destination: `${API_URL}/:path*`,
-            },
-        ]
-    },
-    images: {
-        domains: ['rubyscore.fra1.digitaloceanspaces.com'],
-    },
-}
+  swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Request-Headers',
+            value: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept',
+          },
+          { key: 'Access-Control-Allow-Origin', value: 'https://galxe.com' },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Content-Type', value: 'application/json' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS,DELETE,PUT' },
+        ],
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path(^(?!.*\\/static\\/).*$)',
+        destination: `/api/:path*`,
+      },
+      {
+        source: '/api/:path*',
+        destination: `${API_URL}/:path*`,
+      },
+    ];
+  },
+  images: {
+    domains: ['rubyscore.fra1.digitaloceanspaces.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
