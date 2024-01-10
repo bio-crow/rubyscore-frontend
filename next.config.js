07794c3a0ba84d5880410cb93e3e40103864bc23
rubyscore_tech@lansoft.dev <rubyscore_tech@lansoft.dev>
2024-01-10 23:04:26 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -2,23 +2,7 @@ const API_URL = process.env.NEXT_PUBLIC_DEVELOP_PROXY_BACK_END_API;
 
 const nextConfig = {
   swcMinify: true,
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Request-Headers',
-            value: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization',
-          },
-          { key: 'Access-Control-Allow-Origin', value: 'https://galxe.com' },
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Content-Type', value: 'application/json' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS,DELETE,PUT' },
-        ],
-      },
-    ];
-  },
+
   async rewrites() {
     return [
       {
