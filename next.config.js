5775d4a20c8889e84e47cd59316439dc76f372ae
rubyscore_tech <rubyscore_tech@lansoft.dev>
2024-01-10 17:37:13 +0300

    cors

--- next.config.js
+++ next.config.js
@@ -2,8 +2,25 @@ const API_URL = process.env.NEXT_PUBLIC_DEVELOP_PROXY_BACK_END_API
 
 const nextConfig = {
     swcMinify: true,
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "https://galxe.com/" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                ]
+            }
+        ]
+    },
     async rewrites() {
         return [
+            {
+                source: '/api/:path(^(?!.*\\/static\\/).*$)',
+                destination: `/api/:path*`,
+            },
             {
                 source: '/api/:path*',
                 destination: `${API_URL}/:path*`,
