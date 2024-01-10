44d6adc7b232483fd7a274b4045bdd4a4383236e
rubyscore_tech@lansoft.dev <rubyscore_tech@lansoft.dev>
2024-01-10 22:02:58 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -7,6 +7,8 @@ const nextConfig = {
             {
                 source: "/api/:path*",
                 headers: [
+                    { key: "Access-Control-Request-Headers", value: "X-PINGOTHER, Content-Type" },
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "https://galxe.com" },
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
                 ]
