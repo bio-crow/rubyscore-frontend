0a4e7c92be92b03da45e87bea8d1ce82c44e1385
rubyscore_tech@lansoft.dev <rubyscore_tech@lansoft.dev>
2024-03-18 20:20:57 +0300

    linting

---
+++ lint-staged.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+module.exports = {
+  '**/*.(ts|tsx|js)': filenames => [
+    `yarn eslint --fix ${filenames.join(' ')}`,
+    `yarn prettier --write ${filenames.join(' ')}`,
+  ],
+  '**/*.(md|json)': filenames => `yarn prettier --write ${filenames.join(' ')}`,
+};
+/* eslint-enable */
