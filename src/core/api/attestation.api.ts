8cd2bd6e63fc370dbab9d96a5123bfd73d1db5ce
rubyscore_tech@lansoft.dev <rubyscore_tech@lansoft.dev>
2024-03-22 11:21:32 +0300

    attest limit

--- src/core/api/attestation.api.ts
+++ src/core/api/attestation.api.ts
@@ -1,9 +1,11 @@
-import { apiPrivateAxios } from '@/core/api/axiosConfig';
+import { apiPrivateAxios, apiPrivateAxiosLimited } from '@/core/api/axiosConfig';
 import { IAttestationDataResponse, ITasksResponse } from '@/core/types';
 
 export const fetchAttestationData = async (params: { project: string }) => {
   try {
-    return await apiPrivateAxios.post<IAttestationDataResponse>('/attestation/claim', null, { params });
+    return await apiPrivateAxiosLimited.post<IAttestationDataResponse>('/attestation/claim', null, {
+      params,
+    });
   } catch (error) {
     //console.error(error);
   }
@@ -11,7 +13,9 @@ export const fetchAttestationData = async (params: { project: string }) => {
 
 export const fetchCheckAttestation = async (params: { project: string; wallet: any }) => {
   try {
-    return await apiPrivateAxios.post<IAttestationDataResponse>('/attestation/check', null, { params });
+    return await apiPrivateAxiosLimited.post<IAttestationDataResponse>('/attestation/check', null, {
+      params,
+    });
   } catch (error) {
     //console.error(error);
   }
