32d79f5cc8de9855ba098b2520398b00ae1f5766
rubyscore_tech@lansoft.dev <rubyscore_tech@lansoft.dev>
2024-03-26 10:44:56 +0300

    hide dashboard env

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,22 @@
 'use client';
 import Dashboard from '@/modules/Dashboard/Dashboard';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
 export default function Page() {
+  const isHidden = process.env.NEXT_PUBLIC_HIDE_DASHBOARD === 'true';
+  const { push } = useRouter();
+
+  useEffect(() => {
+    isHidden && push('/');
+  }, []);
   return (
-    <main>
-      <Dashboard />
-    </main>
+    <>
+      {isHidden ? null : (
+        <main>
+          <Dashboard />
+        </main>
+      )}
+    </>
   );
 }
