c91e3a4cf8b4779d99771fcbb4a91b03f310d805
bio-crow <kolesnik.py@gmail.com>
2024-05-27 17:53:30 +0300

    fix: change rank name from zora to taiko

--- src/components/common/sections/ScoreSection/ScoreSection.tsx
+++ src/components/common/sections/ScoreSection/ScoreSection.tsx
@@ -161,7 +161,7 @@ const ScoreSection: FC<Props> = ({
       icon: networkStaticData['taiko'].icon,
       lvl: userScoreList['taiko']?.level,
       points: userScoreList['taiko']?.score,
-      rank: userScoreList['zora']?.rank,
+      rank: userScoreList['taiko']?.rank,
     },
     {
       index: 'mantle',
